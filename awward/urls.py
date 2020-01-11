dd4244d352b3df2e782047a059bde23aad7f5fe9
marysinaida <marydorcassinaida54@gmail.com>
2020-01-11 18:03:18 +0300

    add an ajax url

--- awward/urls.py
+++ awward/urls.py
@@ -9,7 +9,8 @@ urlpatterns=[
     url(r'^project/(\d+)',views.project,name='project'),
     url(r'^new/project$', views.new_project, name='new-project'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_projects,name = 'pastprojects')
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_projects,name = 'pastprojects'),
+    url(r'^ajax/projectsletter/$', views.projectsletter, name='projectsletter')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
