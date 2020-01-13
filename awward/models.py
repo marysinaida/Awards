14e5b91e2f3efc1d1850aadb57f0d27af975a93f
marysinaida <marydorcassinaida54@gmail.com>
2020-01-13 15:43:23 +0300

    add the profile model

--- awward/models.py
+++ awward/models.py
@@ -13,10 +13,11 @@ class Author(models.Model):
     phone_number = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
-            return self.first_name
+        return self.first_name
 
     def save_author(self):
-            self.save()
+        self.save()
+
     class Meta:
         ordering = ['first_name']
 
@@ -24,35 +25,54 @@ class Author(models.Model):
 class Project(models.Model):
     title = models.CharField(max_length=60)
     post = HTMLField()
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     caption = models.CharField(max_length=150)
     pub_date = models.DateTimeField(auto_now_add=True)
-    project_image = models.ImageField(upload_to='projects/',blank=True)
+    project_image = models.ImageField(upload_to='projects/', blank=True)
 
     @classmethod
     def search_by_title(cls, search_term):
         projects = cls.objects.filter(title__icontains=search_term)
         return projects
-    
+
     @classmethod
     def get_projects(cls):
         projects = cls.objects.all()
         return projects
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,)
+    profile_photo = models.ImageField(upload_to='posts/')
+    bio = models.TextField(max_length=255)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def updateProfile(sender, **kwargs):
+        if kwargs['created']:
+            profile = Profile.objects.created(user=kwargs['instance'])
+            post_save.connect(Profile, sender=User)
+
+
 class ProjectsLetterRecipients(models.Model):
-    name = models.CharField(max_length = 30)
+    name = models.CharField(max_length=30)
     email = models.EmailField()
 
     @classmethod
     def todays_projects(cls):
         today = dt.date.today()
-        projects = cls.objects.filter(pub_date__date = today)
+        projects = cls.objects.filter(pub_date__date=today)
         return projects
+
     @classmethod
-    def days_projects(cls,date):
-        projects = cls.objects.filter(pub_date__date = date)
+    def days_projects(cls, date):
+        projects = cls.objects.filter(pub_date__date=date)
         return projects
+
 
 class MoringaMerch(models.Model):
     name = models.CharField(max_length=40)
