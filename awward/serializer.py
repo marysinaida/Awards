1e2825a6028066718a3a27536ee2a1eea70091b2
marysinaida <marydorcassinaida54@gmail.com>
2020-01-14 11:21:31 +0300

    untracked files

--- awward/serializer.py
+++ awward/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Projects
+from .models import Projects,Profile
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Projects
         fields = ('title','post','author','caption','project_link','pub_date','project_image')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields =('user','profile_photo','bio')
